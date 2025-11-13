e73af5652e01acb13d1961e626f5a9ca72643f98
pipogesto <Pipogesto@gmail.com>
2025-11-13 10:59:32 -0600

    Te hace la modificación de. Las imágenes de la página de inicio.

--- js/render.js
+++ js/render.js
@@ -68,12 +68,12 @@ async function renderHomePage(main) {
 
     // Renderizar Categorías
     const categories = [
-        { title: "Medicamentos", description: "Amplia gama de medicamentos", icon: "pill", image: "https://images.unsplash.com/photo-1622147459102-8a0f3727e4c4?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxtZWRpY2luZSUyMHBpbGxzfGVufDF8fHx8MTc1OTIwNzc3OXww&ixlib.rb-4.1.0&q=80&w=1080", color: "#3b82f6" },
-        { title: "Cuidado Personal", description: "Belleza y cuidado de la piel", icon: "sparkles", image: "https://images.unsplash.com/photo-1679394270597-e90694d70350?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxza2luY2FyZSUyMHByb2R1Y3RzfGVufDF8fHx8MTc1OTE0MjM4OXww&ixlib.rb-4.1.0&q=80&w=1080", color: "#ec4899" },
-        { title: "Vitaminas", description: "Suplementos para tu bienestar", icon: "heart", image: "https://images.unsplash.com/photo-1593181581874-361761582b9e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHx2aXRhbWlucyUyMHN1cHBsZW1lbnRzfGVufDF8fHx8MTc1OTE0NzMwNHww&ixlib.rb-4.1.0&q=80&w=1080", color: "#10b981" },
-        { title: "Bebé y Mamá", description: "Cuidado para los más pequeños", icon: "baby", image: "https://images.unsplash.com/photo-1738892248232-a5fd26a98ec4?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxiYWJ5JTIwY2FyZSUyMHByb2R1Y3RzfGVufDF8fHx8MTc1OTIwNzc4MHww&ixlib.rb-4.1.0&q=80&w=1080", color: "#f59e0b" },
-        { title: "Primeros Auxilios", description: "Material de curación y emergencias", icon: "bandage", image: "https://images.unsplash.com/photo-1624638760852-8ede1666ab07?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxmaXJzdCUyMGFpZCUyMGtpdHxlbnwxfHx8fDE3NTkxNDE3NzZ8MA&ixlib.rb-4.1.0&q=80&w=1080", color: "#ef4444" },
-        { title: "Nutrición", description: "Nutrición deportiva y dietética", icon: "salad", image: "https://images.unsplash.com/photo-1593181581874-361761582b9e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHx2aXRhbWlucyUyMHN1cHBsZW1lbnRzfGVufDF8fHx8MTc1OTE0NzMwNHww&ixlib.rb-4.1.0&q=80&w=1080", color: "#8b5cf6" },
+        { title: "Medicamentos", description: "Amplia gama de medicamentos", icon: "pill", image: "imagenes/mediamentos.png", color: "#3b82f6" },
+        { title: "Cuidado Personal", description: "Belleza y cuidado de la piel", icon: "sparkles", image: "imagenes/cuidado-personal.png", color: "#ec4899" },
+        { title: "Vitaminas", description: "Suplementos para tu bienestar", icon: "heart", image: "imagenes/vitaminas.png", color: "#10b981" },
+        { title: "Bebé y Mamá", description: "Cuidado para los más pequeños", icon: "baby", image: "imagenes/bebe-mama.png", color: "#f59e0b" },
+        { title: "Primeros Auxilios", description: "Material de curación y emergencias", icon: "bandage", image: "imagenes/primeros-auxilios.png", color: "#ef4444" },
+        { title: "Nutrición", description: "Nutrición deportiva y dietética", icon: "salad", image: "imagenes/nutricion.png", color: "#8b5cf6" },
     ];
     const categoriesGrid = main.querySelector("#categories-grid-home");
     if(categoriesGrid) {
