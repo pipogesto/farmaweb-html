a548784a00318105a3c94675001ad8c484eddea1
l211080296-gif <l211080296@iztapalapa.tecnm.mx>
2026-02-28 17:31:24 -0600

    Update config.js

--- modules/config.js
+++ modules/config.js
@@ -1,6 +1,7 @@
 // Configuración global de la tienda
 export const SHIPPING_COST = 99;          // Costo de envío estándar
 export const FREE_SHIPPING_THRESHOLD = 900; // Monto mínimo para envío gratis
+export const ADMIN_SECRET = 'farma123';
 
 
 
