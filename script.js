6b21d9b3a16e7c57a4142af0808cea6cd22aae9f
l211080296-gif <l211080296@iztapalapa.tecnm.mx>
2026-02-28 16:39:27 -0600

    Refactor script.js for improved structure and clarity

--- script.js
+++ script.js
@@ -181,12 +181,6 @@ async function handleFormSubmit(event) {
     }
 }
 
-import { auth } from './módulos/auth.js';
-
-// Ejecutar protección de rutas inmediatamente
-auth.protectRoute();
-
-console.log("Farmaweb inicializada con seguridad.");
 
 
 
