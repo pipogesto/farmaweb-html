8673a27a61e01c17e55e4e0ff2ba7ccbab4ef7a1
l211080296-gif <l211080296@iztapalapa.tecnm.mx>
2026-02-28 16:17:36 -0600

    Add MongoDB URI and admin secret to config

--- modules/config.js
+++ modules/config.js
@@ -1,5 +1,8 @@
+export const MONGO_URI = "tu_uri_de_mongodb_atlas"; 
+export const ADMIN_SECRET = "farmaweb_secreto_123"; // Clave para validar al admin
 
 // Configuración global de la tienda
 export const SHIPPING_COST = 99;          // Costo de envío estándar
 
 export const FREE_SHIPPING_THRESHOLD = 900; // Monto mínimo para envío gratis
+
