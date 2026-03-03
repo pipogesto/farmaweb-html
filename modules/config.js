ba2ccb42d25304e669ebe73c971775d77f6e19e2
l211080296-gif <l211080296@iztapalapa.tecnm.mx>
2026-03-03 15:54:04 -0600

    Update config.js

--- modules/config.js
+++ modules/config.js
@@ -2,7 +2,8 @@
 export const SHIPPING_COST = 99;          // Costo de envío estándar
 export const FREE_SHIPPING_THRESHOLD = 900; // Monto mínimo para envío gratis
 export const ADMIN_SECRET = 'farma123';
-export const API_URL = 'https://farmaweb-backend-iwcc.onrender.com';
+export const API_URL = 'https://farmaweb-backend-iwcc.onrender.com/api';
+
 
 
 
