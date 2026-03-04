fa45301778583965f5a433b8082dbf86beac99df
l211080296-gif <l211080296@iztapalapa.tecnm.mx>
2026-03-04 17:15:06 -0600

    Update data.js

--- modules/data.js
+++ modules/data.js
@@ -1,23 +1,21 @@
-import { 'https://farmaweb-backend-iwcc.onrender.com' } from './config.js';
+import { API_URL } from './config.js';
 
 // Intentamos obtener los productos del backend de Render
 export const getAllProducts = async () => {
     try {
-        const response = await fetch(`${ https://farmaweb-backend-iwcc.onrender.com}/api/productos`);
+        // Usamos la variable API_URL que viene de config.js
+        const response = await fetch(`${API_URL}/api/productos`);
+        
         if (!response.ok) throw new Error('Error al conectar con el servidor');
+        
         return await response.json();
     } catch (error) {
         console.error("No se pudieron cargar los productos:", error);
-        return []; // Retorna lista vacía si falla el servidor
+        return []; 
     }
 };
 
-// Mantenemos la lista de usuarios estática por ahora si no tienes ruta de login
 export const allUsers = [
     { email: "cliente@email.com", password: "123", role: "user", name: "Juan Cliente" },
     { email: "admin@farmaplus.com", password: "admin123", role: "admin", name: "Admin FarmaPlus" }
 ];
-
-
-
-
